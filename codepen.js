1c361160e45c470370d6c1b6a7045f9eaf5b18f5
dGrammatiko <d.grammatiko@gmail.com>
2020-06-07 16:34:13 +0200

    Multiple examples pere page
    - Uses a `<codepen-demo></codepen-demo>` Element
    - Use `data-js` on the `<codepen-demo></codepen-demo>` Element for external scripts
    - Use `data-css` on the `<codepen-demo></codepen-demo>` Element for external styles

--- codepen.js
+++ codepen.js
@@ -1,55 +1,86 @@
+function openCodepen(id) {
+  if (!window.pens) return;
+
+  if (window.pens[id]) {
+    const form = document.createElement('form');
+    form.action = "https://codepen.io/pen/define";
+    form.method = "POST";
+    form.target = "_blank";
+    const input = document.createElement('input');
+    input.setAttribute('hidden', '');
+    input.setAttribute('name', 'data');
+    input.setAttribute('value', JSON.stringify(window.pens[id])) //.replace(/"/g, "&​quot;").replace(/'/g, "&apos;"));
+    form.appendChild(input);
+    document.body.appendChild(form);
+    form.submit();
+    document.body.removeChild(form)
+  }
+}
 window.$docsify.plugins = [
-    function (hook, vm) {
-      hook.beforeEach(function (html) {
-        var JSONstring;
-            var form = document.getElementById('demo');
-  
-            if (form) {
-             form.parentNode.removeChild(form);
-            }
+  function (hook, vm) {
+    hook.beforeEach(function (html) {
+      debugger;
+      var JSONstring;
+      var demoRegion = html.match(/<codepen\-demo\s[^>]*>(.*?)<\/codepen\-demo>/gs);
 
-            var HTML = html.match(/```html\n(.*(?:\n(?!```$).*)*)\n```/gm);                  
+      if (demoRegion && demoRegion.length) {
+        window.pens = {}
+        let newHtml = html;
+        demoRegion.forEach((demo, i) => {
+          var HTML = /```html\n(.*(?:\n(?!```$).*)*)\n```/gm.exec(demo);
+          if (HTML && HTML.length) {
             HTML = HTML[0].replace('```html\n', '').replace('\n```', '') || '';
+          }
 
-            var CSS = html.match(/```css\n(.*(?:\n(?!```$).*)*)\n```/gm);
-            CSS = CSS[0].replace('```css\n', '').replace('\n```', '') || '';
+          var CSS = /```css\n(.*(?:\n(?!```$).*)*)\n```/gm.exec(demo);
+          if (CSS && CSS.length) {
+            CSS = CSS.replace('```css\n', '').replace('\n```', '') || '';
+          }
 
-            var JS = html.match(/```js\n(.*(?:\n(?!```$).*)*)\n```/gm);
+          var JS = /```js\n(.*(?:\n(?!```$).*)*)\n```/gm.exec(demo);
+          if (JS && JS.length) {
             JS = JS[0].replace('```js\n', '').replace('\n```', '') || '';
+          }
 
-            var data = {
-                html: HTML,
-                css: CSS,
-                js: JS,
-                title                 : vm.route.file.toLowerCase().replace('.md', ''),
-                description           : "Hands on demo for: " + vm.route.file.toLowerCase().replace('.md', ''),
-                private               : false,
-                editors               : "111",
-                layout                : "top",
-                html_pre_processor    : "none",
-                css_pre_processor     : "none",
-                css_prefix            : "neither",
-                js_pre_processor      : "none",
-                html_classes          : "loading",
-                head                  : "<meta name='viewport' content='width=device-width'>",
-                // css_external          : "http://yoursite.com/style.css", // semi-colon separate multiple files
-                // js_external           : "http://yoursite.com/script.js" // semi-colon separate multiple files
-            };
-  
-            // Quotes will screw up the JSON
-           JSONstring = JSON.stringify(data).replace(/"/g, "&​quot;") .replace(/'/g, "&apos;");
 
-           if (HTML.length || CSS.length || JS.length) {
-            return html + '\n' +
-            '<form id="demo" action="https://codepen.io/pen/define" method="POST" target="_blank">' + 
-              '<input type="hidden" name="data" value=\'' + JSONstring + '\'>' + 
-              '<p>Open in Codepen.io:</p>' + 
-              '<input type="image" src="cp-arrow-right.svg" width="40" height="40" value="Open in Codepen.io" class="codepen-mover-button">' +
-            '</form>';
-           }
+          // data-*
+          const jsLink = /(data-js=")((?:(?!\/>|>|"|\'|\s).)+)/g.exec(demo);
+          const cssLink = /(data-css=")((?:(?!\/>|>|"|\'|\s).)+)/g.exec(demo);
 
-           return html;
-      });
-    }
-  ];
-  
+          var data = {
+            html: HTML,
+            css: CSS,
+            js: JS,
+            title: vm.route.file.toLowerCase().replace('.md', ''),
+            description: "Hands on demo for: " + vm.route.file.toLowerCase().replace('.md', ''),
+            private: false,
+            editors: "111",
+            layout: "top",
+            html_pre_processor: "none",
+            css_pre_processor: "none",
+            css_prefix: "neither",
+            js_pre_processor: "none",
+            html_classes: "loading",
+            head: "<meta name='viewport' content='width=device-width'>",
+          };
+
+          if (jsLink && jsLink.length === 3) {
+            data.js_external = jsLink[2]
+          }
+          if (cssLink && cssLink.length === 3) {
+            data.css_external = cssLink[2]
+          }
+
+          if (HTML.length || CSS.length || JS.length) {
+            window.pens[i] = data;
+
+            newHtml = newHtml.replace(demo, demo + '<button onclick="openCodepen(this.dataset.pen)" data-pen="' + i + '">Open in Codepen</button>');
+          }
+        });
+
+        return newHtml;
+      }
+      return html;
+    });
+  }
+];
